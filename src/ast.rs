b1f5b40ca2621c14dc74efa4cdecf519cefef46f
ppodds <oscar20020629@gmail.com>
2022-12-14 00:04:44 +0800

    feat: implement grammer

---
+++ src/ast.rs
@@ -0,0 +1,52 @@
+pub struct DefineStatement(pub String, pub Box<Expression>);
+pub struct FunctionExpression(pub Vec<String>, pub Box<Expression>);
+
+pub struct IfExpression(
+    pub Box<Expression>,
+    pub Box<Expression>,
+    pub Box<Expression>,
+);
+
+pub enum Statement {
+    Expression(Box<Expression>),
+    DefineStatement(DefineStatement),
+    PrintStatement(PrintStatement),
+}
+
+pub enum PrintStatement {
+    PrintNumber(Box<Expression>),
+    PrintBoolean(Box<Expression>),
+}
+
+pub enum FunctionCall {
+    ExpressionCall(FunctionExpression, Vec<Box<Expression>>),
+    NameCall(String, Vec<Box<Expression>>),
+}
+
+pub enum Expression {
+    Number(i32),
+    Boolean(bool),
+    Variable(String),
+    NumOperate(NumOperator),
+    LogicalOperate(LogicalOperator),
+    FunctionExpression(FunctionExpression),
+    FunctionCall(FunctionCall),
+    IfExpression(IfExpression),
+}
+
+pub enum NumOperator {
+    Plus(Box<Expression>, Vec<Box<Expression>>),
+    Minus(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Vec<Box<Expression>>),
+    Divide(Box<Expression>, Box<Expression>),
+    Modulus(Box<Expression>, Box<Expression>),
+    Greater(Box<Expression>, Box<Expression>),
+    Smaller(Box<Expression>, Box<Expression>),
+    Equal(Box<Expression>, Vec<Box<Expression>>),
+}
+
+pub enum LogicalOperator {
+    And(Box<Expression>, Vec<Box<Expression>>),
+    Or(Box<Expression>, Vec<Box<Expression>>),
+    Not(Box<Expression>),
+}
